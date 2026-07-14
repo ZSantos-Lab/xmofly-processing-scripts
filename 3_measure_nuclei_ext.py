b5135cb51923037ddde6bf8494917c37a1f91463
Nuno Pimpao Martins <nuno.pimpao@gmail.com>
2026-07-14 15:57:01 +0100

    set highest resolution for analysis to 1, for consistency

--- 3_measure_nuclei_ext.py
+++ 3_measure_nuclei_ext.py
@@ -226,7 +226,7 @@ def main(datapath='.', extension='.tif', compute_dask_data=True, resolution_leve
 
     image_processing_props = {
         "resolution_level": resolution_level,
-        "resolution_level_higher": None,
+        "resolution_level_higher": 1, # for nuclei analysis purposes, highest resolution will be 1 for consistency
         "min_voxel_volume": min_voxel_volume,
         "sigma_gaussian": sigma_gaussian,
         "nucleus_channel_min": nucleus_channel_min,
@@ -265,7 +265,7 @@ def main(datapath='.', extension='.tif', compute_dask_data=True, resolution_leve
         image_array = None
 
         #compute new properties for higher resolution
-        image_processing_props['resolution_level_higher'] = resolution_level - 1 if resolution_level > 0 else 0
+        if image_processing_props['resolution_level_higher'] is None: image_processing_props['resolution_level_higher'] = resolution_level - 1 if resolution_level > 0 else 0
         resolution_difference_factor = 2 ** abs(image_processing_props['resolution_level'] - image_processing_props['resolution_level_higher'])
         image_processing_props['min_voxel_volume'] = min_voxel_volume * resolution_difference_factor  # Adjust min_voxel_volume for higher resolution
         image_processing_props['sigma_gaussian'] = sigma_gaussian * resolution_difference_factor  # Adjust sigma for higher resolution
